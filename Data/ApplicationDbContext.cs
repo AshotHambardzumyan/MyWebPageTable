bdb97125356d0c06d89fe16aeb549015266c5653
Աշոտ Համբարձումյան <ashothambardzumyan.tt955-1@polytechnic.am>
2021-06-08 23:32:58 +0400

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MyWebPageTable.Models;
+
+namespace MyWebPageTable.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MyWebPageTable.Models.Person> Person { get; set; }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebPageTable.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Person()
+        {
+
+        }
+    }
+}
